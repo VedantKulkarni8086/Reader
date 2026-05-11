09ad45cb0cf46a3511131d0a5e5b0f16818fe3c3
Vedant Kulkarni <155243263+VedantKulkarni8086@users.noreply.github.com>
2026-05-11 15:29:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import ReaderPage from "./pages/Reader";
 import { ThemeProvider } from "./components/ThemeProvider";
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/reader" element={<ReaderPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
