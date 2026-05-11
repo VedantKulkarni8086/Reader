cada3bdff3c0cee41f77bdb9c739c52d0335e2f5
Vedant Kulkarni <155243263+VedantKulkarni8086@users.noreply.github.com>
2026-05-11 15:30:00 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1 style={{ color: 'white', textAlign: 'center', marginTop: '20%' }}>React is working!</h1>
+    <App />
   </StrictMode>,
 )
